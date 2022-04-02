25d2e4a50b39fb05d249e6c5853a4be84b90ebaa
Aidan Comi <19440113+aidancomi@users.noreply.github.com>
2022-04-02 22:47:21 +0000

    Fixed issue with messages containing commas

--- main.py
+++ main.py
@@ -14,8 +14,10 @@ async def on_message(message):
 
   
   messageIn = message.content #imports message
-  messageIn = messageIn.replace(" ","") #removes spcaes
+  messageIn = messageIn.replace(" ","") #removes spaces
   if re.search(r"\([0-9]+,[0-9]+\)", messageIn): #checks if message matches format
+      match = re.search(r"\([0-9]+,[0-9]+\)", messageIn)
+      messageIn = messageIn[messageIn.find(match.group()):]  
       finalMessage = ("Looks like you are talking about coordinates on r/Place! Allow me to link them! https://www.reddit.com/r/place/?cx="+messageIn[messageIn.find('(') + 1:messageIn.find(',')]+"&cy="+messageIn[messageIn.find(',') +1:messageIn.find(')')]+"&px=10") #Generates message
       await message.channel.send(finalMessage) #sends message
   return
